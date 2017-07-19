a5bdcdfdb5b4805b606af2e514da472b4aa6aad4
Hack Reactor Student <student@hackreactor.com>
2017-07-18 20:11:15 -0700

    backCast

--- src/views/app.js
+++ src/views/app.js
@@ -1,14 +1,56 @@
 var AppView = Backbone.View.extend({
-
+  
   el: '#app',
+
+  // initialize: function(data) {
+  //   this.videos = new Videos(data);
+  //   // this.videos.fetch('barbeque');
+  //   // this.render();
+
+  // },
+
 
   initialize: function() {
     this.videos = new Videos();
+
+    this.listenTo(this.videos, 'sync', this.selectFirst);
+    this.videos.search('javascript tutorial');
+    this.render();
   },
 
 
+  selectFirst: function() {
+    if (this.videos.length > 0) {
+      this.videos.at(0).select();
+    }
+  },
+
+
+
+
   render: function() {
+    
     this.$el.html(this.template());
+
+    // render videoListView
+    new VideoListView({
+      collection: this.videos, // Pass the data from exampleVideoData into the VideoListView
+      el: this.$('.list')
+    }).render();
+    
+    // VideoPlayerView
+    new VideoPlayerView({
+      model: this.videos.at(0), 
+      collection: this.videos,
+      el: this.$('.player')
+    }).render();
+
+    // SearchView
+    new SearchView({
+      collection: this.videos,
+      el: this.$('.search')
+    }).render();
+
     return this;
   },
 
